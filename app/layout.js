cb50ff679e17a88982ac3616580cb7d9cff9f135
Kiran Gautam <gautamkiran878@gmail.com>
2025-01-25 16:27:46 +0545

    Portfoilo Website Using NEXT JS

---
+++ app/layout.js
@@ -0,0 +1,30 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Kiran | Gautam",
+  description: "Welcome to Kiran Gautam's portfolio. I'm a passionate web developer specializing in full-stack development, modern web design, and efficient programming. Explore my projects, skills, and achievements.",
+};
+
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}

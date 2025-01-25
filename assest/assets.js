2e9a7650a16abe25144a70aaef60f987a0bd96ed
Kiran Gautam <gautamkiran878@gmail.com>
2025-01-25 19:18:29 +0545

    All assests done

--- assest/assets.js
+++ assest/assets.js
@@ -35,6 +35,7 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
+import kiran from './kirann.png'
 
 export const assets = {
     user_image,
@@ -47,6 +48,7 @@ export const assets = {
     vscode,
     firebase,
     figma,
+    kiran,
     git,
     mongodb,
     right_arrow_white,
